3492595abb373a786b87450ba4cb37be22654d93
allen-padilla <allenjpadilla@gmail.com>
2022-04-28 14:49:14 -0500

    Fixed display controller Added placing of mark

--- tictactoe.js
+++ tictactoe.js
@@ -1,43 +1,87 @@
-const GameBoard = (() => {
-    //let board = Array(9);
-    let board = ['X','X','O','','','','','',''];
+const gameBoard = (() => {
+  let board = Array(9);
 
-    return {
-        board
-    };
+  const placeMark = (num, mark) => {
+    if (board[num] == undefined) {
+      board[num] = mark;
+    }
+
+    console.log(checkWinner());
+    displayController();
+  }
+
+  const checkWinner = () => {
+    for (let i = 0; i < 3; i++) {
+      // Horizontal
+      if (board[0 + (i * 3)] != undefined && board[0 + (i * 3)] == board[1 + (i * 3)] && board[0 + (i * 3)] == board[2 + (i * 3)]) {
+        return true;
+      }
+
+      // Vertical
+      if (board[0 + i] != undefined && board[0 + i] == board[3 + i] && board[0 + i] == board[6 + i]) {
+        return true;
+      }
+
+      // Left Diagonal
+      if (board[0] != undefined && board[0] == board[4] && board[0] == board[8]) {
+        return true;
+      }
+
+      // Right Diagonal
+      if (board[2] != undefined && board[2] == board[4] && board[2] == board[6]) {
+        return true;
+      }
+    }
+  }
+
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = undefined;
+    }
+  }
+
+  return { board, placeMark, reset };
 })();
 
-const Player = (choice) => {
-    const getChoice = () => choice;
-    const selectChoice = () => {
+const player = () => {
+  let mark = "X";
 
-    }
+  const changeTurn = () => {
+    mark = "0";
+  }
+
+  return { mark, changeTurn }
 }
 
-const displayBoard = () => {
-    let board = GameBoard.board;
-    let display = document.createElement('div');
-    display.setAttribute('class', 'display');
+const displayController = () => {
+  let display = document.createElement('div');
 
-    for(let i = 0; i < board.length; i++){
-        let displayBox = document.createElement('div');
-        displayBox.setAttribute('class', 'box');
+  for (let i = 0; i < gameBoard.board.length; i++) {
+    let box = document.createElement('div');
+    box.setAttribute('class', 'box');
 
-        let fillSpot = document.createElement('p');
-        fillSpot.textContent = board[i];
+    let mark = document.createElement('p');
+    mark.textContent = gameBoard.board[i];
 
-        displayBox.appendChild(fillSpot);
-        display.appendChild(displayBox);
-    }
+    box.appendChild(mark);
+    display.appendChild(box);
+  }
 
-    document.getElementsByClassName('display')[0].innerHTML = display.innerHTML
-    console.log(board);
+  document.getElementsByClassName('display')[0].innerHTML = display.innerHTML;
+  let boxes = document.getElementsByClassName('box');
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', () => {
+      gameBoard.placeMark(i, 'X');
+    })
+  }
 }
+
+(function start(params) {
+  displayController();
+})()
 
 document.getElementById('btn-one-player').addEventListener('click', () => {
-    displayBoard();
-})
-
-document.getElementById('btn-two-players').addEventListener('click', () => {
-    console.log('Player One & Two')
+  gameBoard.reset();
+  displayController();
 })
