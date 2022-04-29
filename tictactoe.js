36162ef642ac83103a0a347d8904a65dbbf2be2d
allen-padilla <allenjpadilla@gmail.com>
2022-04-29 08:32:29 -0500

    Random moves in tic tac toe.

--- tictactoe.js
+++ tictactoe.js
@@ -1,54 +1,101 @@
+
 const gameBoard = (() => {
   let board = Array(9);
 
   const placeMark = (num, mark) => {
     if (board[num] == undefined) {
       board[num] = mark;
+      play.mark = play.mark == 'X' ? 'O' : 'X';
     }
 
-    console.log(checkWinner());
+    if (playerCount == 1 && play.mark == 'O') {
+      ai.botMove();
+    }
+
     displayController();
+
+    if (checkWinner() != null) {
+      let doc = document.createElement('div');
+      doc.setAttribute('class', 'result')
+      if (checkWinner()[0] == 'Winner')
+        doc.textContent = `${checkWinner()[1] == 'X' ? 'Player 1' : 'Player 2'} is the ${checkWinner()[0]}`;
+      else
+        doc.textContent = `Tie!`;
+
+      setTimeout(() => {
+        document.getElementsByClassName('display')[0].innerHTML = doc.innerHTML;
+      }, 300);
+    }
   }
 
   const checkWinner = () => {
+    let moves = 0;
+    for (let i = 0; i < 8; i++) {
+      if (board[i] != undefined) {
+        moves++;
+      }
+    }
+
+    if (moves == board.length) {
+      return ['Tie'];
+    }
+
     for (let i = 0; i < 3; i++) {
       // Horizontal
       if (board[0 + (i * 3)] != undefined && board[0 + (i * 3)] == board[1 + (i * 3)] && board[0 + (i * 3)] == board[2 + (i * 3)]) {
-        return true;
+        return ['Winner', board[0 + (i * 3)]];
       }
 
       // Vertical
       if (board[0 + i] != undefined && board[0 + i] == board[3 + i] && board[0 + i] == board[6 + i]) {
-        return true;
+        return ['Winner', board[0 + i]];
       }
 
       // Left Diagonal
       if (board[0] != undefined && board[0] == board[4] && board[0] == board[8]) {
-        return true;
+        return ['Winner', board[4]];
       }
 
       // Right Diagonal
       if (board[2] != undefined && board[2] == board[4] && board[2] == board[6]) {
-        return true;
+        return ['Winner', board[4]];
       }
     }
   }
 
   const reset = () => {
+    play = player('X');
+
     for (let i = 0; i < board.length; i++) {
       board[i] = undefined;
     }
   }
 
-  return { board, placeMark, reset };
+  return { board, placeMark, checkWinner, reset };
 })();
 
-const player = () => {
-  let mark = "X";
+const ai = (() => {
+  const botMove = () => {
+    let legalMoves = Array();
+    for (let i = 0; i < gameBoard.board.length; i++) {
+      if (gameBoard.board[i] === undefined) {
+        legalMoves.push(i)
+      }
+    }
 
-  const changeTurn = () => {
-    mark = "0";
+    if (gameBoard.checkWinner() == null) {
+      setTimeout(() => {
+        gameBoard.placeMark(legalMoves[Math.floor(Math.random() * legalMoves.length)], play.mark);
+      }, 300);
+    }
   }
+
+  return { botMove }
+})()
+
+const player = (mark) => {
+  const changeTurn = () => {
+  };
 
   return { mark, changeTurn }
 }
@@ -72,16 +119,26 @@ const displayController = () => {
 
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('click', () => {
-      gameBoard.placeMark(i, 'X');
+      gameBoard.placeMark(i, play.mark);
     })
   }
 }
+
+let play = player('X');
+let playerCount = 1;
 
 (function start(params) {
   displayController();
 })()
 
 document.getElementById('btn-one-player').addEventListener('click', () => {
+  playerCount = 1;
+  gameBoard.reset();
+  displayController();
+})
+
+document.getElementById('btn-two-players').addEventListener('click', () => {
+  playerCount = 2;
   gameBoard.reset();
   displayController();
 })
